ad0349f89579ed30136d96d5d53f3bb9fd1ec6c3
Miguel Cabeza <miguel567@gmail.com>
2020-08-21 00:12:12 +0200

    inserting in DB correctly

--- notifier.js
+++ notifier.js
@@ -21,7 +21,25 @@ if (result.error) {
 }
 
 
-async function getGasPrice (){
+// Connect to DB
+let conn;
+
+mysql.createConnection({   
+    host: process.env.host, 
+    user: process.env.user, 
+    password: process.env.password, 
+    database: process.env.database
+    }).then((val,err) =>{
+        if (!err){
+            conn = val;
+            logger.debug(conn);
+            logger.info('connected to DB');
+  
+        } else {logger.error(err);}
+    });
+
+//Fetch Gas Price Data
+async function getGasPrice (conn){
     try {
       const response = await axios.get('https://data-api.defipulse.com/api/v1/egs/api/ethgasAPI.json?api-key=' + process.env.GASSTATION_API_KEY)
       var gasPriceData = {
@@ -36,12 +54,16 @@ async function getGasPrice (){
           standardTime: '<5m',
           lowTime: '<30m'
       };
-      logger.info({Fast: gasPriceData.fast + gasPriceData.fastTime, Standard: gasPriceData.standard + gasPriceData.standardTime, Low: gasPriceData.low + gasPriceData.lowTime, Fastest: gasPriceData.fastest, BlockNumber: gasPriceData.blocknum, BlockTime: gasPriceData.blocktime, Speed: gasPriceData.speed});
-
+      logger.info({Fast: gasPriceData.fast + gasPriceData.fastTime, Standard: gasPriceData.standard + gasPriceData.standardTime, Low: gasPriceData.low + gasPriceData.lowTime, Fastest: gasPriceData.fastest, BlockNumber: gasPriceData.blocknum, BlockTime: gasPriceData.blockTime, Speed: gasPriceData.speed});
+      //Inser price in DB
+      var now = moment().format('YYYY-MM-DD HH:mm:ss');
+      await conn.execute("INSERT INTO gasPrice (timestamp, blocknumber, fastest, fast, standard, low, blocktime, speed) VALUES (?,?,?,?,?,?,?,?)",[now,gasPriceData.blocknum,gasPriceData.fastest,gasPriceData.fast,gasPriceData.standard,gasPriceData.low,gasPriceData.blockTime,gasPriceData.speed]);
+      logger.debug({TIMESTAMP: now}, 'INSERTED in DB');
     } catch (error) {
-        logger.error(error.response.body);
+        logger.error(error);
     }
   }
 
-  var requestLoop = setInterval(function(){ getGasPrice()}, 3000);
+//Generate loop every X seconds to fetch data 
+var requestLoop = setInterval(function(){ getGasPrice(conn)}, 20000);
 
